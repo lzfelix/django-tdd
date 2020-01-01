da140e8e600342732f31e0ae0727bceaf84d5ab9
Luiz Felix <lzcfelix@gmail.com>
2020-01-01 18:45:13 -0300

    Adding todo list text input

--- functional_tests.py
+++ functional_tests.py
@@ -39,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathres' for row in rows)
+            any(row.text == '1: Buy peacock feathres' for row in rows),
+            'New to-do item not in the table'
         )
 
         # There's still a text box to add another item. User adds
