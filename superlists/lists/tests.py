c1713769343fca34fc365142d891dfdf8e644510
Luiz Felix <lzcfelix@gmail.com>
2020-01-03 20:11:59 -0300

    Adding Item model to ORM and testing it

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,7 +6,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-
+from lists.models import Item
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -45,3 +45,19 @@ class HomePageTest(TestCase):
     def remove_csrf_tag(text: str) -> str:
         """Remove csrf tag from text. See https://groups.google.com/forum/#!topic/obey-the-testing-goat-book/fwY7ifEWKMU"""
         return re.sub(r'<[^>]*csrfmiddlewaretoken[^>]*>', '', text)
+
+class ItemModelTest(TestCase):
+    def test_saving_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item, the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(saved_items[0].text, first_item.text)
+        self.assertEqual(saved_items[1].text, second_item.text)
